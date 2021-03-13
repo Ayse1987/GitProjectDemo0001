734bced20c881ae99b9c230310b63f38ffac6993
Ayse1987 <79757064+Ayse1987@users.noreply.github.com>
2021-03-13 20:32:41 +0000

    Update FirstMethod.java
    changes on remote ayse branch

--- src/package1/FirstMethod.java
+++ src/package1/FirstMethod.java
@@ -5,6 +5,8 @@ public class FirstMethod {
 	public static void main(String[] args) {
 		System.out.println("Ayse");
 		//ayse local changes
+		//here is the conflicting line
+		//I will cay remote masret
 
 	}
 
